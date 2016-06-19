0acb15ce2aeab4a37fc1a8f43b7b3897ec542da6
Dan Bourque <Dan.Bourque@gmail.com>
2016-06-19 08:41:49 -0700

    Nearly done with the mini-me movements

--- Assets/Scripts/ProxyUpdater.cs
+++ Assets/Scripts/ProxyUpdater.cs
@@ -2,17 +2,13 @@
 using System.Collections;
 
 public class ProxyUpdater: MonoBehaviour{
-	private const float realX =	0.42f;
-	private const float realY =	-0.375f;
-	private const	float realZ =	1.29f;
-	private const	float miniX = 0.00556f;
-	private const	float miniY = -0.00356f;
-	private const	float miniZ = -0.00695f;
-	private const	float ratioX = ( miniX/realX )*10;
-	private const	float ratioY = ( miniY/realY )*10;
-	private const	float ratioZ = ( miniZ/realZ )*10;
+	private const float scaleRatio = 0.0554648f;
+
+	private const float offsetX = -0.49781264f;
+	private const float offsetY = -0.3559896f;
+	private const float offsetZ = 0.737765f;
 
 	public static Vector3 MiniaturizePosition( Vector3 realWorldPos ){
-		return new Vector3( realWorldPos.x * ratioX, realWorldPos.y * ratioY, realWorldPos.z * ratioZ );
+		return new Vector3( ( realWorldPos.x*scaleRatio )+offsetX, ( realWorldPos.y*scaleRatio )+offsetY, ( realWorldPos.z*scaleRatio )+offsetZ );
 	}
 }
